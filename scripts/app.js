8c3aa2e9c826aaa5a91f10829aa9bd942aaeb90b
CPrich905 <charlie.prichard@gmail.com>
2019-12-12 14:41:52 +0000

    Buttons added for start/stop/reset - console logs result

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,16 @@ const width = 20
 // import './stylesheets/style.scss'
 const squares = []
 
+
 function init() {
+  const startBtn = document.querySelector('#startBtn')
+  const stopBtn = document.querySelector('#stopBtn')
+  const resetBtn = document.querySelector('#resetBtn')
   const grid = document.querySelector('.grid')
+ 
+  startBtn.addEventListener('click', play)
+  stopBtn.addEventListener('click', stop)
+  resetBtn.addEventListener('click', reset)
   
   for (let i = 0; i < width*width; i++) {
     const square = document.createElement('div')
@@ -12,6 +20,16 @@ function init() {
     square.innerHTML = i
     grid.append(square)
   }
+}
+
+function play() {
+  console.log('clicked start')
+}
+function stop() {
+  console.log('clicked stop')
+}
+function reset() {
+  console.log('clicked reset')
 }
 
 window.addEventListener('DOMContentLoaded', init)
