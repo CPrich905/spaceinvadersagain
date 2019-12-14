a96f5aeaa671c462dab3cf720dde81392d19e9cc
CPrich905 <charlie.prichard@gmail.com>
2019-12-14 14:39:56 +0000

    added notes, player class, key listeners for movement. movePlayer() doesn't fire

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,43 @@
 const width = 20
 const squares = []
+const player = null
+
+class Player{
+  constructor(startingIndex) {
+    this.playerIndex = startingIndex
+    this.lives = 3
+    this.hit = false
+  }
+
+  // player movement
+  movePlayer() {
+    console.log('move player fires')
+    this.startingIndex = Math.floor(width * width - width)
+    squares.forEach(square => square.classList.remove('player'))
+    squares[this.playerIndex].classList.add('player')
+  }
+}
+
+function keyHandler() {
+  let playerMoves = true
+  
+  switch(expectExport.keyCode) {
+    case 39:
+      if (player.playerIndex % width < width-1) {
+        player.playerIndex++
+      }
+      break
+    case 37:
+      if (player.playerIndex % width > 0) {
+        player.playerIndex--
+      }
+      break
+    
+    default:
+      playerMoves: false
+  }
+  if (playerMoves) player.movePlayer()
+}
 
 // Init creates grid, attached event listeners to start/stop/play buttons.
 function init() {
@@ -16,18 +54,26 @@ function init() {
     const square = document.createElement('div')
     square.classList.add('grid-item')
     squares.push(square)
-    square.innerHTML = i
     grid.append(square)
   }
   return squares
 }
-
+// play needs to populate aliens array, player & start timers for move/fire
+// alien rank = (width/3) then (rank/2) * ranks
+// movement function should have each alien move across the grid until their count reaches (width - ranks)
+// alienshoot must ensure that not all aliens fire simultaneously (?Math.random?)
 function play() {
   console.log('clicked start')
+  rank = Math.floor(width/3)
+  row = Math.floor(rank/2)
+  const player = new Player(width*width-width)
 }
+
+// should pause all timers & disable player move/fire controls, change innerHTML to restart
 function stop() {
   console.log('clicked stop')
 }
+// should reload page
 function reset() {
   console.log('clicked reset')
 }
@@ -35,4 +81,14 @@ function reset() {
 
 window.addEventListener('DOMContentLoaded', init)
 
-//UPDATES FOR CLIFF:
+// UPDATES FOR CLIFF:
+// X added notes and tested ranks/squares/width in start()
+// X build class for player
+// X add listeners for keydowns on arrows & shoot buttons
+// - check player moves
+// X remove i from grid-items
+
+
+// IDEAS
+// christmas theme?
+// missile count for players/weapon cooldown. Different type of weapons?
